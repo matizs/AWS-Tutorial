b08fe01813ed94c07e31d516a465a5a99ce85521
jaehyukjang <culdm@naver.com>
2021-09-08 21:33:56 +0900

    [AWS_Tutorial] 테스트코드 변경 AWS 키 제거

--- src/test/java/com/example/Tutorial/SqsServiceTest/SqsServiceTest.java
+++ src/test/java/com/example/Tutorial/SqsServiceTest/SqsServiceTest.java
@@ -7,16 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 public class SqsServiceTest {
 
-    @DisplayName("Test Test")
+    @DisplayName("테스트용 테스트")
     @Test
     public void Test(){
         assertTrue(true);
         assertEquals(1, 1);
     }
 
-    @DisplayName("Test Test2")
+    @DisplayName("테스트용 테스트2")
     @Test
     public void Tes2(){
-        assertEquals(5, 6);
+        assertEquals(5, 5);
     }
 }
